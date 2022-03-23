48605d8aaccf39382ff748411ecbf73106d57a46
amit <amitpay1234@gmail.com>
2022-03-23 20:11:27 +0200

    Second commit for first part of the assigment tests are done.

--- Demo.cpp
+++ Demo.cpp
@@ -8,27 +8,21 @@
 
 #include "Notebook.hpp"
 #include "Direction.hpp"
+
 using ariel::Direction;
 
 #include <iostream>
 #include <stdexcept>
+
 using namespace std;
 
 int main() {
-	ariel::Notebook notebook;
-	notebook.write(/*page=*/100, /*row=*/100, /*column=*/50, Direction::Horizontal, "abcd");
-	cout << notebook.read(/*page=*/100, /*row=*/99, /*column=*/51, Direction::Vertical, /*length=*/3) << endl;  
-		// prints "_b_" (starts at row 99 which is empty; then at row 100 there is "b"; then row 101 is empty again).
-	notebook.write(/*page=*/100, /*row=*/99, /*column=*/52, Direction::Vertical, "xyz");
-		// throws error because writing the letter y will intersect with the letter c
-	notebook.erase(/*page=*/100, /*row=*/99, /*column=*/51, Direction::Vertical, /*length=*/3);
-		// writes ~ instead of _ in line 99, b in line 100 and _ again in line 99
-
-	notebook.show(100); // shows the notebook's page in a reasonable way. For example:
-		//    98:  _________
-		//    99:  ___~_____
-		//    100: __a~cd___
-		//    101: ___~_____
-		//    102: _________
+    ariel::Notebook notebook;
+    notebook.write(11, 14, 5, Direction::Horizontal,
+                   "412442");
+    notebook.write(11, 18, 5, Direction::Horizontal,
+                   "41242");
+    notebook.write(11, 15, 30, Direction::Vertical, "abcde");
+    notebook.erase(11, 17, 30, ariel::Direction::Vertical, 9);
+    notebook.show(11);
 }
-
--- Test.cpp
+++ Test.cpp
@@ -7,46 +7,47 @@
 
 using namespace ariel;
 
-TEST_CASE("bad input"){
-    SUBCASE("out of bounds"){
+TEST_CASE ("bad input") {
+            SUBCASE("out of bounds") {
         Notebook notebook;
-        CHECK_THROWS(notebook.write(50,90,105,ariel::Direction::Horizontal,"out of bound"));
-        CHECK_THROWS(notebook.write(50,90,200,ariel::Direction::Vertical,"out of bound"));
-        CHECK_THROWS(notebook.read(10,10,105,Direction::Vertical,30));
-        CHECK_THROWS(notebook.read(10,10,105,Direction::Horizontal,30));
-        CHECK_THROWS(notebook.erase(50,20,10,ariel::Direction::Horizontal,105));
-        CHECK_THROWS(notebook.erase(50,20,60,ariel::Direction::Horizontal,46));
-        CHECK_THROWS(notebook.write(101,90,105,ariel::Direction::Horizontal,"out of bound"));
-        CHECK_THROWS(notebook.write(102,90,200,ariel::Direction::Vertical,"out of bound"));
-        CHECK_THROWS(notebook.read(103,10,105,Direction::Vertical,30));
-        CHECK_THROWS(notebook.read(104,102,1205,Direction::Horizontal,30));
-        CHECK_THROWS(notebook.erase(50,20,150,ariel::Direction::Horizontal,105));
-        CHECK_THROWS(notebook.erase(50,220,610,ariel::Direction::Horizontal,46));
+        CHECK_THROWS(notebook.write(50, 90, 105, ariel::Direction::Horizontal, "out of bound"));
+        CHECK_THROWS(notebook.write(50, 90, 200, ariel::Direction::Vertical, "out of bound"));
+        CHECK_THROWS(notebook.read(10, 10, 105, Direction::Vertical, 30));
+        CHECK_THROWS(notebook.read(10, 10, 105, Direction::Horizontal, 30));
+        CHECK_THROWS(notebook.erase(50, 20, 10, ariel::Direction::Horizontal, 105));
+        CHECK_THROWS(notebook.erase(50, 20, 60, ariel::Direction::Horizontal, 46));
+        CHECK_THROWS(notebook.write(101, 90, 105, ariel::Direction::Horizontal, "out of bound"));
+        CHECK_THROWS(notebook.write(102, 90, 200, ariel::Direction::Vertical, "out of bound"));
+        CHECK_THROWS(notebook.read(103, 10, 105, Direction::Vertical, 30));
+        CHECK_THROWS(notebook.read(104, 102, 1205, Direction::Horizontal, 30));
+        CHECK_THROWS(notebook.erase(50, 20, 150, ariel::Direction::Horizontal, 105));
+        CHECK_THROWS(notebook.erase(50, 220, 610, ariel::Direction::Horizontal, 46));
     }
-    SUBCASE("negative input"){
+            SUBCASE("negative input") {
         Notebook notebook;
-        CHECK_THROWS(notebook.write(-10,10,10,Direction::Horizontal,"123"));
-        CHECK_THROWS(notebook.write(10,-10,10,Direction::Horizontal,"123"));
-        CHECK_THROWS(notebook.write(10,10,-10,Direction::Horizontal,"123"));
-        CHECK_THROWS(notebook.read(-10,10,10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.read(10,-10,10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.read(10,10,-10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.read(10,10,10,Direction::Horizontal,-10));
-        CHECK_THROWS(notebook.erase(-10,10,10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.erase(10,-10,10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.erase(10,10,-10,Direction::Horizontal,10));
-        CHECK_THROWS(notebook.erase(10,10,10,Direction::Horizontal,-10));
+        CHECK_THROWS(notebook.write(-10, 10, 10, Direction::Horizontal, "123"));
+        CHECK_THROWS(notebook.write(10, -10, 10, Direction::Horizontal, "123"));
+        CHECK_THROWS(notebook.write(10, 10, -10, Direction::Horizontal, "123"));
+        CHECK_THROWS(notebook.read(-10, 10, 10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.read(10, -10, 10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.read(10, 10, -10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.read(10, 10, 10, Direction::Horizontal, -10));
+        CHECK_THROWS(notebook.erase(-10, 10, 10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.erase(10, -10, 10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.erase(10, 10, -10, Direction::Horizontal, 10));
+        CHECK_THROWS(notebook.erase(10, 10, 10, Direction::Horizontal, -10));
     }
 
 }
-TEST_CASE("good input"){
+
+TEST_CASE ("good input") {
     Notebook notebook;
-    CHECK_NOTHROW(notebook.write(10,10,10,ariel::Direction::Horizontal,"best test"));
-    CHECK_NOTHROW(notebook.write(20,20,20,ariel::Direction::Horizontal,"int the world"));
-    CHECK_NOTHROW(notebook.write(30,30,30,ariel::Direction::Horizontal,"give me 100"));
-    CHECK_NOTHROW(notebook.write(40,40,40,ariel::Direction::Vertical,"I LOVE BOYS"));
-    CHECK_NOTHROW(notebook.erase(40,40,40,ariel::Direction::Vertical,11));
-    CHECK_NOTHROW(notebook.read(10,10,10,ariel::Direction::Horizontal,9));
-    CHECK_NOTHROW(notebook.read(20,20,20,ariel::Direction::Horizontal,13));
-    CHECK_NOTHROW(notebook.read(30,30,30,ariel::Direction::Horizontal,11));
+    CHECK_NOTHROW(notebook.write(10, 10, 10, ariel::Direction::Horizontal, "best test"));
+    CHECK_NOTHROW(notebook.write(20, 20, 20, ariel::Direction::Horizontal, "int the world"));
+    CHECK_NOTHROW(notebook.write(30, 30, 30, ariel::Direction::Horizontal, "give me 100"));
+    CHECK_NOTHROW(notebook.write(40, 40, 40, ariel::Direction::Vertical, "I LOVE BOYS"));
+    CHECK_NOTHROW(notebook.erase(40, 40, 40, ariel::Direction::Vertical, 11));
+    CHECK_NOTHROW(notebook.read(10, 10, 10, ariel::Direction::Horizontal, 9));
+    CHECK_NOTHROW(notebook.read(20, 20, 20, ariel::Direction::Horizontal, 13));
+    CHECK_NOTHROW(notebook.read(30, 30, 30, ariel::Direction::Horizontal, 11));
 }
